1d3073cdd32fd0779abd4349619fbfe69d9d1331
mizchi <miz404@gmail.com>
2023-08-30 17:03:09 +0900

    format

--- src/worker.ts
+++ src/worker.ts
@@ -9,35 +9,39 @@ export interface Env {
 let _smc: SourceMapConsumer = null as any;
 async function loadSMC(bucket: R2Bucket) {
 	if (_smc) return _smc;
-	const mapFileBody = await bucket.get('worker.js.map');
+	const mapFileBody = await bucket.get("worker.js.map");
 	const map = await mapFileBody?.json();
 	const consumer = new SourceMapConsumer(map as RawSourceMap);
 	_smc = consumer;
-	return consumer
+	return consumer;
 }
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+	async fetch(
+		request: Request,
+		env: Env,
+		ctx: ExecutionContext,
+	): Promise<Response> {
 		try {
-			if (request.url.endsWith('/error')) {
-				throw new Error('manual-error');
+			if (request.url.endsWith("/error")) {
+				throw new Error("manual-error");
 			}
-			if (request.url.endsWith('/direct-error')) {
-				throw new Error('direct-error');
+			if (request.url.endsWith("/direct-error")) {
+				throw new Error("direct-error");
 			}
-			return new Response('Hello World!');
+			return new Response("Hello World!");
 		} catch (err) {
-			if (err instanceof Error && err.message === 'manual-error') {
+			if (err instanceof Error && err.message === "manual-error") {
 				const smc = await loadSMC(env.SCM_BUCKET);
 				const frames = ErrorStackParser.parse(err);
-				const originalFrames = frames.map(frame => {
+				const originalFrames = frames.map((frame) => {
 					return smc.originalPositionFor({
 						line: frame.lineNumber!,
 						column: frame.columnNumber!,
 					});
 				});
 				// build error format
-				let out = "Error: " + err.message + '\n';
+				let out = "Error: " + err.message + "\n";
 				for (const f of originalFrames) {
 					out += `  at ${f.source}:${f.line}:${f.column}\n`;
 				}
